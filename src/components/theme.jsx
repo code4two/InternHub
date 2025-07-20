47faa0bdbd4f592154246cb9a40d97d0369d8744
Peter Adepoju <peteradepoju22@gmail.com>
2025-07-20 02:41:47 +0100

    changed font size

--- src/components/theme.jsx
+++ src/components/theme.jsx
@@ -1,6 +1,6 @@
 const Theme = ({ title, Icon }) => {
   return (
-    <div className='flex flex-row gap-2 py-3 px-2 lg:px-4 w-full cursor-pointer items-center text-xl xl:text-2xl border border-blue-700 font-semibold'>
+    <div className='flex flex-row gap-2 py-3 px-2 lg:px-4 w-full cursor-pointer items-center text-xl border border-blue-700 font-semibold'>
         <div>{Icon && <Icon />}</div>
         <div>{title}</div>
     </div>
